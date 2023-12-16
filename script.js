2b3ed89165c54c50be7aa813624b7e8ae54ee115
Mads Madsen <madsen.mads@gmail.com>
2023-12-16 15:57:47 +0100

    simplify url function to find bugs

--- script.js
+++ script.js
@@ -110,21 +110,29 @@ function getCategoryIndex(exerciseName) {
     return Object.keys(exercises).findIndex(category => exercises[category].includes(exerciseName));
 }
 
-// Updated generateTableFromUrl function
+// Function to generate table from URL parameters with debugging
 function generateTableFromUrl() {
     const urlParams = new URLSearchParams(window.location.search);
     const planParam = urlParams.get("plan");
+    console.log("Plan from URL:", planParam); // Debugging: Log the plan parameter
+
     if (!planParam) return;
 
     const workoutTable = document.getElementById("workout-table");
-    if (!workoutTable) return;
+    if (!workoutTable) {
+        console.log("Workout table not found."); // Debugging: Check if table is found
+        return;
+    }
 
-    planParam.split(",").forEach(pair => {
-        const [categoryIndex, exerciseIndex] = pair.split("-").map(Number);
-        const categoryName = Object.keys(exercises)[categoryIndex];
-        const exerciseName = exercises[categoryName][exerciseIndex];
-        addExerciseToPlan(exerciseName);
-    });
+    // Simplified handling: Process only the first pair for debugging
+    const firstPair = planParam.split(",")[0];
+    const [categoryIndex, exerciseIndex] = firstPair.split("-").map(Number);
+    const categoryName = Object.keys(exercises)[categoryIndex];
+    const exerciseName = exercises[categoryName][exerciseIndex];
+
+    console.log("Adding exercise from URL:", categoryName, exerciseName); // Debugging: Log the exercise being added
+
+    addExerciseToPlan(exerciseName);
 }
 
 
